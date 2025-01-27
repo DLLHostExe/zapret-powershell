2257471f8f6b11ce17d847e4bd7edc9d2034d2be
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-27 22:10:27 +0700

    Update zapret-canary.ps1

--- zapret-canary.ps1
+++ zapret-canary.ps1
@@ -1,17 +1,13 @@
 # bol-van, sevcator and others & made by <3
 Clear-Host
 $folderPath = "C:\Windows\Zapret"
-$hostlist = "--hostlist-exclude=`"$folderPath\exclude.txt`" --hostlist-auto=`"$folderPath\autohostlist.txt`""
+$hostlist = "--hostlist-exclude=`"$folderPath\list-exclude.txt`" --hostlist-auto=`"$folderPath\list-auto.txt`""
 $ARGS = "--wf-tcp=80,443 --wf-udp=80,443,50000-50099 "
-$ARGS += "--filter-tcp=80 --dpi-desync=fake,fakedsplit --dpi-desync-autottl=2 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" $hostlist --new "
-$ARGS += "--filter-tcp=443 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" --new "
-$ARGS += "--filter-tcp=80 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake,multidisorder --dpi-desync-split-pos=1,midsld --dpi-desync-repeats=11 --dpi-desync-fooling=md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" --new "
-$ARGS += "--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig --dpi-desync-fake-tls=`"$folderPath\tls.bin`" $hostlist --new "
-$ARGS += "--filter-udp=443 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$folderPath\quic.bin`" --new "
-$ARGS += "--filter-udp=80 --hostlist=`"$folderPath\google.txt`" --dpi-desync=fake --dpi-desync-repeats=11 --dpi-desync-fake-quic=`"$folderPath\quic.bin`" --new "
-$ARGS += "--filter-udp=80 --dpi-desync=fake --dpi-desync-repeats=11 $hostlist --new "
-$ARGS += "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $hostlist --new "
-$ARGS += "--filter-udp=50000-50099 --ipset=`"$folderPath\ipset-discord.txt`" --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-any-protocol --dpi-desync-cutoff=n4"
+$ARGS += "--filter-udp=443 $hostlist --dpi-desync=fake --dpi-desync-repeats=6 --dpi-desync-fake-quic=`"$folderPath\quic-google.bin`" --new "
+$ARGS += "--filter-udp=443 --dpi-desync=fake --dpi-desync-repeats=11 $autohostlist --new "
+$ARGS += "--filter-udp=50000-50100 --ipset=$MODPATH/ipset-discord.txt --dpi-desync=fake --dpi-desync-any-protocol --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new "
+$ARGS += "--filter-tcp=443 $hostlist --dpi-desync=fake --dpi-desync-autottl=2 --dpi-desync-repeats=6 --dpi-desync-fooling=badseq --dpi-desync-fake-tls=`"$folderPath\tls-google.bin`" "
+$ARGS += "--filter-tcp=443 --dpi-desync=fake,multidisorder --dpi-desync-split-pos=midsld --dpi-desync-repeats=6 --dpi-desync-fooling=badseq,md5sig $autohostlist --new"
 Write-Host "  ______                    _                                          "
 Write-Host " |___  /                   | |                                         "
 Write-Host "    / / __ _ _ __  _ __ ___| |_ ______ ___ __ _ _ __   __ _ _ __ _   _ "
@@ -20,7 +16,7 @@ Write-Host "  / /_| (_| | |_) | | |  __/ |_      | (_| (_| | | | | (_| | |  | |_
 Write-Host " /_____\__,_| .__/|_|  \___|\__|      \___\__,_|_| |_|\__,_|_|   \__, |"
 Write-Host "            | |                                                   __/ |"
 Write-Host "            |_|                                                  |___/ "
-Write-Host "Advanced installation of bol-van software @ follow sevcator.t.me >_< !!"
+Write-Host "Installation with addons of bol-van software. @ follow sevcator.t.me !!"
 function Check-Admin {
     $identity = [System.Security.Principal.WindowsIdentity]::GetCurrent()
     $principal = New-Object System.Security.Principal.WindowsPrincipal($identity)
@@ -55,64 +51,30 @@ if (Test-Path "$folderPath\uninstall.cmd") {
 }
 # Source: GitHub - censorliber/zapret
 function Set-DNS {
-    $provider = ""
-
-    if (-not $provider) {
-        $provider = Read-Host "- Enter DNS provider (google/cloudflare/dnssb): "
-    }
-
-    $primaryDNS = ""
-    $secondaryDNS = ""
-    $primaryDNSv6 = ""
-    $secondaryDNSv6 = ""
-
-    switch ($provider) {
-        "google" {
-            $primaryDNS = "8.8.8.8"
-            $secondaryDNS = "8.8.4.4"
-            $primaryDNSv6 = "2001:4860:4860::8888"
-            $secondaryDNSv6 = "2001:4860:4860::8844"
-        }
-        "cloudflare" {
-            $primaryDNS = "1.1.1.1"
-            $secondaryDNS = "1.0.0.1"
-            $primaryDNSv6 = "2606:4700:4700::1111"
-            $secondaryDNSv6 = "2606:4700:4700::1001"
-        }
-        "dnssb" {
-            $primaryDNS = "185.222.222.222"
-            $secondaryDNS = "185.184.222.222"
-            $primaryDNSv6 = "2a09::"
-            $secondaryDNSv6 = "2a09::1"
-        }
-        default {
-            Write-Host "- Error: Unsupported DNS provider $provider. Supported options are google, cloudflare, or dnssb" -ForegroundColor Yellow
-            return
-        }
-    }
+    $primaryDNS = "185.222.222.222"
+    $secondaryDNS = "45.11.45.11"
 
     try {
-        $interfaces = Get-NetAdapter | Where-Object {$_.Status -eq "Up"} -ErrorAction Stop
+        $interfaces = Get-NetAdapter | Where-Object { $_.Status -eq "Up" } -ErrorAction Stop
     } catch {
-        Write-Host "- Unable to retrieve active network interfaces: $_" -ForegroundColor Yellow
+        Write-Host "- Failed to get active network adapters: $_" -ForegroundColor Yellow
         return
     }
 
     if ($interfaces.Count -eq 0) {
-        Write-Host "- No active network interfaces found" -ForegroundColor Yellow
+        Write-Host "- The network adapters not found" -ForegroundColor Red
         return
     }
 
     foreach ($interface in $interfaces) {
         try {
-            Write-Host "- Setting DNS for $($interface.InterfaceAlias): IPv4 ($primaryDNS, $secondaryDNS)"
+            Write-Host "- Configuring DNS for $($interface.InterfaceAlias)"
             Set-DnsClientServerAddress -InterfaceAlias $interface.InterfaceAlias -ServerAddresses $primaryDNS, $secondaryDNS -ErrorAction Stop
-            if ($primaryDNSv6 -and $secondaryDNSv6) {
-                Write-Host "- Setting DNS for $($interface.InterfaceAlias): IPv6 ($primaryDNSv6, $secondaryDNSv6)"
-                Set-DnsClientServerAddress -InterfaceAlias $interface.InterfaceAlias -ServerAddresses $primaryDNSv6, $secondaryDNSv6 -ErrorAction Stop
-            }
+
+            Write-Host "- Disabling IPv6 for $($interface.InterfaceAlias)"
+            Disable-NetAdapterBinding -Name $interface.InterfaceAlias -ComponentID ms_tcpip6 -ErrorAction Stop
         } catch {
-            Write-Host "- Failed to set DNS server for $($interface.InterfaceAlias): $_" -ForegroundColor Yellow
+            Write-Host "- Failed set configuration to adapter $($interface.InterfaceAlias): $_" -ForegroundColor Yellow
         }
     }
 }
@@ -180,11 +142,10 @@ $files = @(
     @{Url = "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/cygwin1.dll"; Name = "cygwin1.dll"},
     @{Url = "https://github.com/bol-van/zapret-win-bundle/raw/refs/heads/master/zapret-winws/winws.exe"; Name = "winws.exe"},
     @{Url = "https://raw.githubusercontent.com/bol-van/zapret-win-bundle/refs/heads/master/zapret-winws/ipset-discord.txt"; Name = "ipset-discord.txt"},
-    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/autohostlist.txt"; Name = "autohostlist.txt"},
-    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/google.txt"; Name = "google.txt"},
-    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/exclude.txt"; Name = "exclude.txt"},
-    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/tls_clienthello_www_google_com.bin"; Name = "tls.bin"}
-    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/quic_initial_www_google_com.bin"; Name = "quic.bin"}
+    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/list.txt"; Name = "list.txt"},
+    @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/list-exclude.txt"; Name = "list-exclude.txt"},
+    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/tls_clienthello_www_google_com.bin"; Name = "tls-google.bin"}
+    @{Url = "https://github.com/bol-van/zapret/raw/refs/heads/master/files/fake/quic_initial_www_google_com.bin"; Name = "quic-google.bin"}
     @{Url = "https://raw.githubusercontent.com/sevcator/zapret-powershell/refs/heads/main/files/uninstall.cmd"; Name = "uninstall.cmd"}
 )
 foreach ($file in $files) {
@@ -202,6 +163,24 @@ try {
 } catch {
     Write-Host ("Failed to create or start service: {0}" -f $_.Exception.Message) -ForegroundColor Red
 }
+function hosts-Config {
+    $hostsFile = "C:\Windows\System32\drivers\etc\hosts"
+    $canaryHostsFile = "canary-hosts"
+
+    Write-Host "- Modifying hosts"
+    
+    if (Test-Path $canaryHostsFile) {
+        try {
+            $canaryHosts = Get-Content -Path $canaryHostsFile -ErrorAction Stop
+            Add-Content -Path $hostsFile -Value $canaryHosts
+        } catch {
+            Write-Host "- Error to add hosts from $canaryHostsFile: $_" -ForegroundColor Yellow
+        }
+    } else {
+        Write-Host "- The file $canaryHostsFile not found" -ForegroundColor Yellow
+    }
+}
+hosts-Config
 Write-Host "- Done!"
 Write-Host "- To remove Zapret, run script located in $folderPath\uninstall.cmd as administrator!"
 Write-Host "*** sevcator.t.me / sevcator.github.io ***"
