d851e68c649e8e77efee5f2ea80066a3721e2b71
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-27 22:15:34 +0700

    Update uninstall.cmd

--- files/uninstall.cmd
+++ files/uninstall.cmd
@@ -2,13 +2,12 @@
 
 net session >nul 2>&1
 if %errorLevel% neq 0 (
-    echo You must run this script with administrative privileges to countinue.
+    echo - You must run this script with administrative privileges to continue.
     exit /b
 )
 
-cd "C:\Windows\System32"
+cd "C:\Windows\Zapret" >nul 2>&1
 taskkill /F /IM winws.exe /T >nul 2>&1
-taskkill /F /IM dnscrypt-proxy.exe /T >nul 2>&1
 
 net stop winws1 >nul 2>&1
 net stop windivert >nul 2>&1
@@ -16,16 +15,15 @@ net stop windivert14 >nul 2>&1
 sc stop zapret >nul 2>&1
 sc stop windivert >nul 2>&1
 sc stop windivert14 >nul 2>&1
-sc stop dnscrypt-proxy >nul 2>&1
 sc delete winws1 >nul 2>&1
 sc delete windivert >nul 2>&1
 sc delete windivert14 >nul 2>&1
-sc delete dnscrypt-proxy >nul 2>&1
 
 set "folderPath=C:\Windows\Zapret"
 set "tempPath=C:\Windows\Temp\Zapret"
 
 if exist "%folderPath%" (
+    if not exist "%tempPath%" mkdir "%tempPath%" >nul 2>&1
     move "%folderPath%\*.txt" "%tempPath%" >nul 2>&1
     rd /s /q "%folderPath%" >nul 2>&1
     mkdir "%folderPath%" >nul 2>&1
@@ -34,9 +32,21 @@ if exist "%folderPath%" (
 )
 
 for /f "tokens=2 delims=," %%i in ('wmic nic where "NetConnectionStatus=2" get NetConnectionID ^| findstr /r /v "^$"') do (
-    netsh interface ipv4 set dns name="%%i" source=dhcp
-    netsh interface ipv6 set dns name="%%i" source=dhcp
-    echo DNS settings reverted for interface: %%i
+    netsh interface ipv4 set dns name="%%i" source=dhcp >nul 2>&1
+    if %errorLevel% equ 0 (
+        echo - DNS settings reverted for interface: %%i
+    ) else (
+        echo - Failed to revert DNS settings for interface: %%i
+    )
 )
 
-echo Done
+set "hostsFile=C:\Windows\System32\drivers\etc\hosts"
+set "canaryHostsFile=canary-hosts"
+if exist "%hostsFile%" if exist "%canaryHostsFile%" (
+    findstr /v /g:%canaryHostsFile% %hostsFile% > "%hostsFile%.tmp"
+    move /y "%hostsFile%.tmp" "%hostsFile%" >nul 2>&1
+    echo - Removed canary-hosts entries from %hostsFile%
+)
+
+echo - Done
+exit /b
