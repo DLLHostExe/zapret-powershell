0df9b8e2a60390c7482c567a1e5c60dd5ebc7a96
Seva <68725282+sevcator@users.noreply.github.com>
2025-01-27 23:06:25 +0700

    Update uninstall.cmd

--- files/uninstall.cmd
+++ files/uninstall.cmd
@@ -6,8 +6,24 @@ if %errorLevel% neq 0 (
     exit /b
 )
 
-cd "C:\Windows\Zapret" >nul 2>&1
-taskkill /F /IM winws.exe /T >nul 2>&1
+for /f "tokens=*" %%a in ('netsh interface show interface ^| findstr /i "Ethernet"') do (
+    set adapter=%%a
+    set adapter=!adapter:~0,15!
+    netsh interface ipv4 set address name="!adapter!" source=dhcp >nul 2>&1
+    netsh interface ipv6 set address name="!adapter!" source=dhcp >nul 2>&1
+)
+
+netsh interface ipv6 set teredo disabled >nul 2>&1
+
+for /f "delims=" %%i in ('findstr /i "C:\\Windows\\Zapret\\canary-hosts" C:\\Windows\\System32\\drivers\\etc\\hosts') do (
+    set "line=%%i"
+    setlocal enabledelayedexpansion
+    set "line=!line:C:\Windows\Zapret\canary-hosts=!"
+    echo !line!>> C:\Windows\System32\drivers\etc\hosts.new
+    endlocal
+)
+
+move /y C:\Windows\System32\drivers\etc\hosts.new C:\Windows\System32\drivers\etc\hosts >nul 2>&1
 
 net stop winws1 >nul 2>&1
 net stop windivert >nul 2>&1
@@ -19,34 +35,8 @@ sc delete winws1 >nul 2>&1
 sc delete windivert >nul 2>&1
 sc delete windivert14 >nul 2>&1
 
-set "folderPath=C:\Windows\Zapret"
-set "tempPath=C:\Windows\Temp\Zapret"
-
-if exist "%folderPath%" (
-    if not exist "%tempPath%" mkdir "%tempPath%" >nul 2>&1
-    move "%folderPath%\*.txt" "%tempPath%" >nul 2>&1
-    rd /s /q "%folderPath%" >nul 2>&1
-    mkdir "%folderPath%" >nul 2>&1
-    move "%tempPath%\*.txt" "%folderPath%" >nul 2>&1
-    rd /s /q "%tempPath%" >nul 2>&1
-)
-
-for /f "tokens=2 delims=," %%i in ('wmic nic where "NetConnectionStatus=2" get NetConnectionID ^| findstr /r /v "^$"') do (
-    netsh interface ipv4 set dns name="%%i" source=dhcp >nul 2>&1
-    if %errorLevel% equ 0 (
-        echo - DNS settings reverted for interface: %%i
-    ) else (
-        echo - Failed to revert DNS settings for interface: %%i
-    )
-)
-
-set "hostsFile=C:\Windows\System32\drivers\etc\hosts"
-set "canaryHostsFile=canary-hosts"
-if exist "%hostsFile%" if exist "%canaryHostsFile%" (
-    findstr /v /g:%canaryHostsFile% %hostsFile% > "%hostsFile%.tmp"
-    move /y "%hostsFile%.tmp" "%hostsFile%" >nul 2>&1
-    echo - Removed canary-hosts entries from %hostsFile%
+for %%f in (C:\Windows\Zapret\*) do (
+    if /i not "%%~xf"==".txt" del /f "%%f" >nul 2>&1
 )
 
 echo - Done
-exit /b
